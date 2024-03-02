030986f88cfa917f0f0c9c31f79d9ad94d3d8e29
Ify0 <87445524+Ify0@users.noreply.github.com>
2024-03-02 12:59:06 +0000

    Made some changes

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -344,7 +344,6 @@ public class EmployeeDetails extends JFrame implements ActionListener, ItemListe
 		if (isSomeoneToDisplay())
 			new EmployeeSummaryDialog(getAllEmloyees());
 	}// end displaySummaryDialog
-
 	// display search by ID dialog
 	private void displaySearchByIdDialog() {
 		if (isSomeoneToDisplay())
@@ -571,9 +570,9 @@ public class EmployeeDetails extends JFrame implements ActionListener, ItemListe
 	}// end deleteDecord
 
 	// create vector of vectors with all Employee details
-	private Vector<Object> getAllEmloyees() {
+	private Vector<Vector<Object>> getAllEmloyees() {
 		// vector of Employee objects
-		Vector<Object> allEmployee = new Vector<Object>();
+		Vector<Vector<Object>> allEmployee = new Vector<Vector<Object>>();
 		Vector<Object> empDetails;// vector of each employee details
 		long byteStart = currentByteStart;
 		int firstId;
